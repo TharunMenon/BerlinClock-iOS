127767874a8c4c3fe95508763b52e7810d37d590
Tharun Menon <Tharunmenon@192.168.1.2>
2026-02-17 19:42:44 +0530

    chore: add Xcode project scaffold

---
+++ BerlinClock/BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+//  Created by Tharun Menon on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerlinClock/BerlinClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BerlinClock
+//
+//  Created by Tharun Menon on 17/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
